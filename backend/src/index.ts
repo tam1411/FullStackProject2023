27454984fa28efae7c2019b64d41a96d68b58c3b
Casey Bailey <caseybailey@gmail.com>
2023-01-31 20:59:16 -0600

    Fastify and middleware in

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
-
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
 import cors from "cors";
 
 import fs from "fs/promises";
 import path from "path";
-import {Nastify} from "./nastify";
-//import { Fastify } from "./fastify";
+//import {Nastify} from "./nastify";
+import Fastify from "fastify";
+import fastifyMiddie from "@fastify/middie"
 
-const app = Nastify();
+const app = Fastify();
+// lookie, now we have top level await!
+await app.register(fastifyMiddie);
 
 console.log(app);
 
@@ -17,88 +20,45 @@ app.use("/about", cors());
 app.use("/get", cors());
 
 app.get("/about", (req, res) => {
-    res.send("I am the about page");
+	res.send("I am the about page");
 });
 
 app.post("/about", (req, res) => {
-    res.send("I am POST REQUEST");
+	res.send("I am POST REQUEST");
 })
 
 app.get("/users", (req, res) => {
 
 })
 
-
-/*
-
-app.get('/users", (req, res) => {
-
-}
-
-app.post('/users", (req, res) => {
-
-}
-
-app.put('/users", (req, res) => {
-
-}
- */
-
 app.get("/", async (req, res) => {
 
-    const indexFile = await fs.readFile(path.resolve(__dirname, 'public', 'index.html'))
-        .catch(err => {
-            console.error(err);
-            //send error result - 500!
-            res.setHeader('Content-Type', 'text/html');
-            res.status(500).send("Error occurred", err);
-        });
+	const indexFile = await fs.readFile(path.resolve(__dirname, 'public', 'index.html'))
+		.catch(err => {
+			console.error(err);
+			//send error result - 500!
+			res.header('Content-Type', 'text/html');
+			res.status(500).send("Error occurred");
+		});
 
-    res.status(200).send(indexFile);
+	res.status(200).send(indexFile);
 });
 
 app.get('/get', async (req, res) => {
-    const indexFile = await fs.readFile(path.resolve(__dirname, 'public', 'index.html'))
-        .catch(err => {
-            console.error(err);
-            //send error result - 500!
-            res.setHeader('Content-Type', 'text/html');
-            return res.status(500).send("Error occurred", err);
+	const indexFile = await fs.readFile(path.resolve(__dirname, 'public', 'index.html'))
+		.catch(err => {
+			console.error(err);
+			//send error result - 500!
+			res.header('Content-Type', 'text/html');
+			return res.status(500).send("Error occurred");
 
-        });
+		});
 
-    return res.status(200).send(indexFile);
+	return res.status(200).send(indexFile);
 });
 
-async function main() {
-    const server = await app.listen(8080, () => {
-        console.log("Server is running");
-    });
-}
 
-void main();
+void await app.listen(8080, () => {
+	console.log("Server is running");
+});
 
-// import {App} from "./app";
-// import {FastifyInstance} from "fastify";
-//
-// const app: FastifyInstance = App({
-//     logger: {
-//         level: 'info',
-//         transport: {
-//             target: 'pino-pretty'
-//         }
-//     }
-// });
-//
-//
-// const host_addr = '0.0.0.0';
-// app.listen({port: 3000, host: host_addr},
-//     (err, _address) => {
-//         if (err) {
-//             app.log.error(err);
-//             process.exit();
-//         }
-//         const msg = `Server listening on '${host_addr}' ...`;
-//         console.log(msg);
-//         app.log.info(msg);
-//     });
